10a5b025a3c79a1f79e767e7174cac174a256711
Bill Sanders <billysanders@gmail.com>
2026-07-02 23:48:04 -0700

    port borealix to new image template

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,6 +22,9 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+mkdir /code
+mkdir /nix
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
